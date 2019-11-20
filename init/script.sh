11464cae1925fb950f4fcf7c0e4300d78521a8ea
yannhowe <yannhowe@gmail.com>
2019-11-20 21:35:22 +0800

    added macos support

--- init/script.sh
+++ init/script.sh
@@ -1,5 +1,13 @@
-chmod +x ./mc
-./mc config host add localhost http://$MINIO_HOST $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
-./mc admin config set localhost < minio.config
-./mc admin service restart localhost
-python ./init.py
+if [ "$(uname)" == "Darwin" ]; then
+    chmod +x ./mc_macos
+    ./mc_macos config host add localhost http://$MINIO_HOST $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+    ./mc_macos admin config set localhost < minio.config
+    ./mc_macos admin service restart localhost
+    python ./init.py
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    chmod +x ./mc_linux
+    ./mc_linux config host add localhost http://$MINIO_HOST $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+    ./mc_linux admin config set localhost < minio.config
+    ./mc_linux admin service restart localhost
+    python ./init.py
+fi
